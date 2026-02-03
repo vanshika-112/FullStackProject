a78193cc2bf8d6d9f43117997d9a296eb08bae19
vanshika-112 <avanshi2211@gmail.com>
2026-02-03 17:58:30 +0530

    added authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def test_api(request):
     return Response({"message" : "API is working"})
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
