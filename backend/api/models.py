c95a8dca4dffabdf2e523883199d6a00866840af
vanshika-112 <avanshi2211@gmail.com>
2026-02-03 18:05:24 +0530

    added dataset kmodels for csv uploads

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Dataset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='datasets/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    total_count = models.IntegerField(null=True, blank=True)
+    averages = models.JSONField(null=True, blank=True)
+    equipment_distribution = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.file.name}"
